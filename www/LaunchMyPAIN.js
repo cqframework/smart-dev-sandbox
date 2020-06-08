059c22206486fce7371d2fa2deabce34ea386df2
bryant <bryant@alphora.com>
2020-06-08 08:43:22 -0600

    adding call to mypain

--- www/LaunchMyPAIN.js
+++ www/LaunchMyPAIN.js
@@ -1,7 +1,7 @@
-function launchPMDashboard(document, dashboardURL, fhirServiceUrl){
+function launchMyPAIN(document, myPainURL, fhirServiceUrl){
         let patientIdEntry = document.getElementById('patientIdEntry').value;
         const launchParams = {a:"1",b:patientIdEntry,e:"efb5d4ce-dffc-47df-aa6d-05d372fdb407",f:"1"}
-        let url1 = dashboardURL + `?launch=${btoa(JSON.stringify(launchParams))}`;
+        let url1 = myPainURL + `?launch=${btoa(JSON.stringify(launchParams))}`;
         console.log(url1);
         window.open(url1, "_blank");
 }

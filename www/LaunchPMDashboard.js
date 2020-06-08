b015c4ba0460b42d296a2b7bcd0529703fa7b707
bryant <bryant@alphora.com>
2020-06-08 15:10:57 -0600

    added launching of MyPAIN

--- www/LaunchPMDashboard.js
+++ www/LaunchPMDashboard.js
@@ -2,6 +2,7 @@ function launchPMDashboard(document, dashboardURL, fhirServiceUrl){
         let patientIdEntry = document.getElementById('patientIdEntry').value;
         const launchParams = {a:"1",b:patientIdEntry,e:"efb5d4ce-dffc-47df-aa6d-05d372fdb407",f:"1"}
         let url1 = dashboardURL + `?launch=${btoa(JSON.stringify(launchParams))}`;
+
         console.log(url1);
         window.open(url1, "_blank");
 }

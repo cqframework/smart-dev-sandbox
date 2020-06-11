91aa7dc04263a5ebe745063454027d5d85d59552
bryant <bryant@alphora.com>
2020-06-11 12:20:46 -0600

    fixed fhir server address on aws; pulled passing the fhir server inside the  launch param

--- www/LaunchPMDashboard.js
+++ www/LaunchPMDashboard.js
@@ -1,7 +1,7 @@
 function launchPMDashboard(document, dashboardURL, fhirServiceUrl){
         let patientIdEntry = document.getElementById('patientIdEntry').value;
-        const launchParams = {a:"1",b:patientIdEntry,e:"efb5d4ce-dffc-47df-aa6d-05d372fdb407",f:"1",q:fhirServiceUrl}
-        let url1 = dashboardURL + `?launch=${btoa(JSON.stringify(launchParams))}`;
+        const launchParams = {a:"1",b:patientIdEntry,e:"efb5d4ce-dffc-47df-aa6d-05d372fdb407",f:"1"}
+        let url1 = dashboardURL + `?launch=${btoa(JSON.stringify(launchParams))}` + `&iss=` + fhirServiceUrl;;
 
         console.log(url1);
         window.open(url1, "_blank");
